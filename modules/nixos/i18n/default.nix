675540b585f6721e41cf1ec11abd151a814c3ffa
rafaelcga <arzemis14@gmail.com>
2025-10-05 01:26:53 +0200

    Simplify locale settings

--- modules/nixos/i18n/default.nix
+++ modules/nixos/i18n/default.nix
@@ -14,17 +14,7 @@ in
   config = lib.mkIf cfg.enable {
     i18n = {
       defaultLocale = cfg.locale;
-      extraLocaleSettings = {
-        LC_ADDRESS = cfg.locale;
-        LC_IDENTIFICATION = cfg.locale;
-        LC_MEASUREMENT = cfg.locale;
-        LC_MONETARY = cfg.locale;
-        LC_NAME = cfg.locale;
-        LC_NUMERIC = cfg.locale;
-        LC_PAPER = cfg.locale;
-        LC_TELEPHONE = cfg.locale;
-        LC_TIME = cfg.locale;
-      };
+      extraLocaleSettings.LC_ALL = cfg.locale;
     };
   };
 }
