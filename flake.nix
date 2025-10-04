1e57a3253fc83caf9aa805f5a9b99489fa6dc6cc
rafaelcga <arzemis14@gmail.com>
2025-10-05 00:21:15 +0200

    nixfmt

--- flake.nix
+++ flake.nix
@@ -3,12 +3,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.fractal = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./hosts/fractal/base.nix
-        ./hosts/fractal/configuration.nix
-      ];
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations.fractal = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./hosts/fractal/base.nix
+          ./hosts/fractal/configuration.nix
+        ];
+      };
     };
-  };
 }
