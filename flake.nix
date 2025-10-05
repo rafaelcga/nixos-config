564fc81b56e6529a22467b8f4797c2e3b88084f3
rafaelcga <arzemis14@gmail.com>
2025-10-05 12:30:45 +0200

    home-manager initial setup

--- flake.nix
+++ flake.nix
@@ -1,15 +1,41 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
     {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      # Set default formatter for `nix fmt`
+      formatter.${system} = pkgs.nixfmt-tree;
       nixosConfigurations.fractal = nixpkgs.lib.nixosSystem {
         modules = [
           ./hosts/fractal/config.nix
           ./hosts/fractal/configuration.nix
+          {
+            home-manager.nixosModules.home-manager = {
+              backupFileExtension = "bak";
+              users.rafael = {
+                imports = [ ];
+                home.stateVersion = 25.11;
+              };
+              useGlobalPkgs = true;
+              useUserPackages = true;
+            };
+          }
         ];
       };
     };
