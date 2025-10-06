059ea36b1bef64a0f21c35a2b2a3e17b2c47eb5a
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-06 20:35:15 +0200

    fix error in attribute height

--- modules/nixos/audio/default.nix
+++ modules/nixos/audio/default.nix
@@ -29,13 +29,13 @@ in
           ];
         };
       };
-    };
-    extraConfig.pipewire."92-low-latency" = {
-      "context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 32;
-        "default.clock.min-quantum" = 32;
-        "default.clock.max-quantum" = 32;
+      extraConfig.pipewire."92-low-latency" = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 32;
+          "default.clock.min-quantum" = 32;
+          "default.clock.max-quantum" = 32;
+        };
       };
     };
   };
