ade1de3f5130bb73550416fd7afda27f73cc1c21
rafaelcga <arzemis14@gmail.com>
2025-10-06 11:40:28 +0200

    Automated function imports and enabled them to be accessed from the top-level

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,17 @@
 { lib, ... }:
-{
-  utils = import ./utils.nix { inherit lib; };
-}
+let
+  excludedFiles = [
+    "default.nix"
+    "extended-lib.nix"
+  ];
+
+  allFiles = builtins.attrNames (builtins.readDir ./.);
+  nixFiles = builtins.filter (
+    name: lib.hasSuffix ".nix" name && !(lib.elem name excludedFiles)
+  ) allFiles;
+  nixNames = builtins.map (name: lib.removeSuffix ".nix" name) nixFiles;
+
+  modules = lib.genAttrs nixNames (name: import ./${name}.nix { inherit lib; });
+  moduleFunctions = lib.mergeAttrsList (lib.attrValues modules);
+in
+moduleFunctions // modules # modules take precedence in merge
