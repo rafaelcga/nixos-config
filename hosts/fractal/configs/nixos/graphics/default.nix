43f12b8e6ade63803a1c83fe4ee0726fc233dca5
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-08 01:34:53 +0200

    fix

--- hosts/fractal/configs/nixos/graphics/default.nix
+++ hosts/fractal/configs/nixos/graphics/default.nix
@@ -2,8 +2,8 @@
 {
   # Ensure always using the same DP numeration
   boot.kernelParams = [
-    "video=DP-1:e"
+    # "video=DP-1:e"
     "video=DP-2:e"
-    # "video=DP-3:e"
+    "video=DP-3:e"
   ];
 }
