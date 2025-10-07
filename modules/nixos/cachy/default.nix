69b914b1c49a936a94324f4a5f81cabb412adcd8
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-07 17:56:36 +0200

    sysctl and udev rules

--- modules/nixos/cachy/default.nix
+++ modules/nixos/cachy/default.nix
@@ -15,12 +15,22 @@ in
 
   config = lib.mkIf cfg.enable {
     boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos;
-    services.ananicy = {
-      enable = true;
-      package = pkgs.ananicy-cpp;
-      rulesProvider = pkgs.ananicy-rules-cachyos;
+    services = {
+      udev = {
+        enable = true;
+        packages = [ cachyos-settings ];
+      };
+      ananicy = {
+        enable = true;
+        package = pkgs.ananicy-cpp;
+        rulesProvider = pkgs.ananicy-rules-cachyos;
+      };
     };
-    environment.etc."security/limits.d/20-audio.conf".source =
-      "${cachyos-settings}/etc/security/limits.d/20-audio.conf";
+    environment.etc = {
+      "sysctl.d/99-cachyos-settings.conf".source =
+        "${cachyos-settings}/usr/lib/sysctl.d/99-cachyos-settings.conf";
+      "security/limits.d/20-audio.conf".source =
+        "${cachyos-settings}/etc/security/limits.d/20-audio.conf";
+    };
   };
 }
