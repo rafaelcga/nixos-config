178c7f59af6551840418a56dbe2f01ffd9fcbd77
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-07 21:21:04 +0200

    gnome packages

--- modules/nixos/desktop/gnome/default.nix
+++ modules/nixos/desktop/gnome/default.nix
@@ -15,62 +15,65 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    services.displayManager.gdm.enable = true;
-    services.desktopManager.gnome.enable = true;
-    environment.gnome.excludePackages = with pkgs; [
-      gnome-tour
-      gnome-user-docs
-      baobab
-      decibels
-      epiphany
-      gnome-characters
-      gnome-clocks
-      gnome-console
-      gnome-contacts
-      gnome-font-viewer
-      gnome-logs
-      gnome-maps
-      gnome-music
-      gnome-system-monitor
-      gnome-weather
-      gnome-connections
-      gnome-software
-      simple-scan
-      snapshot
-      totem
-      yelp
-      evince
-      geary
-    ];
-    # Add packages replacing GNOME ones
-    environment.systemPackages =
-      with pkgs;
-      [
-        ghostty
-        papers
-        celluloid
-        cosmic-store
-      ]
-      ++ extensions;
-    programs.nautilus-open-any-terminal = {
-      enable = true;
-      terminal = "ghostty";
+    services = {
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
     };
-    programs.dconf.profiles.user.databases = [
-      {
-        settings = {
-          "org/gnome/mutter" = {
-            experimental-features = [
-              "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
-              "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
-              "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
-            ];
+    environment = {
+      gnome.excludePackages = with pkgs; [
+        gnome-tour
+        gnome-user-docs
+        baobab
+        decibels
+        epiphany
+        gnome-characters
+        gnome-clocks
+        gnome-console
+        gnome-contacts
+        gnome-font-viewer
+        gnome-logs
+        gnome-maps
+        gnome-music
+        gnome-system-monitor
+        gnome-weather
+        gnome-connections
+        gnome-software
+        simple-scan
+        snapshot
+        totem
+        yelp
+        evince
+        geary
+      ];
+      systemPackages =
+        with pkgs;
+        [
+          ghostty
+          papers
+          celluloid
+          cosmic-store
+        ]
+        ++ extensions;
+      sessionVariables.NIXOS_OZONE_WL = "1";
+    };
+    programs = {
+      xwayland.enable = true;
+      dconf.profiles.user.databases = [
+        {
+          settings = {
+            "org/gnome/mutter" = {
+              experimental-features = [
+                "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+                "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+                "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+              ];
+            };
+            "org/gnome/shell" = {
+              enabled-extensions = extensionsUuid;
+            };
           };
-          "org/gnome/shell" = {
-            enabled-extensions = extensionsUuid;
-          };
-        };
-      }
-    ];
+        }
+      ];
+    };
   };
 }
