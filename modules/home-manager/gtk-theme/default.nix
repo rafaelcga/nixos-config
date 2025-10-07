9ab574e21f2e9344abf29cd240d66db931c24669
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-07 12:29:49 +0200

    simplify gtk theme

--- modules/home-manager/gtk-theme/default.nix
+++ modules/home-manager/gtk-theme/default.nix
@@ -19,22 +19,11 @@ in
       };
     };
     gtk = {
-      enable = true;
-      gtk3 = {
-        enable = true;
-        theme = {
-          name = "Adwaita-dark";
-          package = pkgs.gnome-themes-extra;
-        };
-        extraConfig = {
-          gtk-application-prefer-dark-theme = 1;
-        };
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
       };
-      gtk4 = {
-        enable = true;
-        extraConfig = {
-          gtk-application-prefer-dark-theme = 1;
-        };
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
       };
     };
     qt = lib.mkIf (!config.catppuccin.enable) {
