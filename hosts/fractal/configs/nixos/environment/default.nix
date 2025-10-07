e824670bccb5d0e7ec2fb2642d08ee8df3061628
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-07 20:30:27 +0200

    environment variables

--- hosts/fractal/configs/nixos/environment/default.nix
+++ hosts/fractal/configs/nixos/environment/default.nix
@@ -3,5 +3,7 @@
   environment.sessionVariables = {
     GSK_RENDERER = "gl";
     GDK_SCALE = "1.25";
+    __GL_SHADER_DISK_CACHE_SIZE = "12000000000";
+    MESA_SHADER_CACHE_MAX_SIZE = "12G";
   };
 }
