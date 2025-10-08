cc66e090cf9293ced779501013e876e71ed53854
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-09 00:05:36 +0200

    chaotic nyx cachix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
   nixConfig = {
     extra-substituters = [
-      "https://nix-community.cachix.org"
       "https://chaotic-nyx.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
     ];
   };
