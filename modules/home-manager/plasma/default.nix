dc2b41f6cb3cbd732a8fd5240fd7e25b124c9734
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-08 19:58:46 +0200

    NixOS logo splash

--- modules/home-manager/plasma/default.nix
+++ modules/home-manager/plasma/default.nix
@@ -7,19 +7,27 @@
 let
   cfg = config.modules.home-manager.plasma;
   usesCatppuccin = config.catppuccin.enable or false;
-  colorScheme =
-    if usesCatppuccin then
-      "Catppuccin"
-      + (lib.local.capitalizeFirst config.catppuccin.flavor)
-      + (lib.local.capitalizeFirst config.catppuccin.accent)
-    else
-      "BreezeDark";
+  upperFlavor = lib.local.capitalizeFirst config.catppuccin.flavor or "";
+  upperAccent = lib.local.capitalizeFirst config.catppuccin.accent or "";
+
+  colorScheme = if usesCatppuccin then "Catppuccin${upperFlavor}${upperAccent}" else "BreezeDark";
+  globalThemeName = if usesCatppuccin then "Catppuccin-${upperFlavor}-${upperAccent}" else "";
+
+  nixosLogoPath = ../../../resources/icons/nix-snowflake-rainbow-pastel.png;
+  originalLogoPath = "/share/plasma/look-and-feel/${globalThemeName}/contents/splash/images/Logo.png";
   catppuccinKde = lib.mkIf usesCatppuccin (
-    pkgs.catppuccin-kde.override {
-      flavour = [ config.catppuccin.flavor ];
-      accents = [ config.catppuccin.accent ];
-    }
+    (pkgs.catppuccin-kde.overrideAttrs (old: {
+      postInstall = (old.postInstall or "") + ''
+        cp ${nixosLogoPath} $out${originalLogoPath}
+      '';
+    })).override
+      {
+        flavour = [ config.catppuccin.flavor ];
+        accents = [ config.catppuccin.accent ];
+      }
   );
+  splashTheme = if usesCatppuccin then "${globalThemeName}-splash" else "Breeze";
+
   fontConfig = {
     family = "JetBrainsMono Nerd Font";
     pointSize = 12;
@@ -39,7 +47,7 @@ in
       enable = true;
       workspace = {
         inherit colorScheme;
-        splashScreen.theme = "None"; # No splash screen
+        splashScreen.theme = splashTheme;
         windowDecorations = {
           theme = themeConfig.name;
           inherit (themeConfig) library;
