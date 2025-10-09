f25b4214c03d55918be8188d282e943500eb6f7f
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-09 20:18:11 +0200

    listNixPaths

--- lib/default.nix
+++ lib/default.nix
@@ -1,17 +1,29 @@
 { lib, ... }:
 let
-  excludedFiles = [
-    "default.nix"
-    "extended-lib.nix"
-  ];
+  # Function that returns all .nix files and modules (directories containing a
+  # default.nix) in a given rootDir; can pass file or directory names to exclude.
+  listNixPaths =
+    {
+      rootDir ? ./.,
+      exclude ? [
+        "default.nix"
+        "extended-lib.nix"
+      ],
+    }:
+    let
+      isNixFile = { item, type }: (lib.hasSuffix ".nix" item) && (type == "regular");
+      isModule =
+        { item, type }: (builtins.pathExists rootDir + "${item}/default.nix") && (type == "directory");
+    in
+    (builtins.attrNames (
+      lib.filterAttrs (
+        item: type:
+        (isNixFile { inherit item type; } || isModule { inherit item type; }) && !(lib.elem item exclude)
+      ) (builtins.readDir rootDir)
+    ));
 
-  allFiles = builtins.attrNames (builtins.readDir ./.);
-  nixFiles = builtins.filter (
-    name: lib.hasSuffix ".nix" name && !(lib.elem name excludedFiles)
-  ) allFiles;
-  nixNames = builtins.map (name: lib.removeSuffix ".nix" name) nixFiles;
-
-  modules = lib.genAttrs nixNames (name: import ./${name}.nix { inherit lib; });
+  nixFileStems = builtins.map (fileName: lib.removeSuffix ".nix" fileName) (listNixPaths { });
+  modules = lib.genAttrs nixFileStems (name: import ./${name}.nix { inherit lib; });
   moduleFunctions = lib.mergeAttrsList (lib.attrValues modules);
 in
-moduleFunctions // modules # modules take precedence in merge
+moduleFunctions // { inherit listNixPaths; } // modules # modules take precedence in merge
