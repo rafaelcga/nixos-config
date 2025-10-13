91a611db3c878f6e5b5c9a4f63a93c7009c4605b
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-13 10:09:52 +0200

    update lock

--- modules/common/sops/default.nix
+++ modules/common/sops/default.nix
@@ -11,7 +11,7 @@ in
   sops = {
     defaultSopsFile = ../../../hosts/${hostName}/secrets.yaml;
     age.sshKeyPaths = [
-      "/etc/ssh/ssh_host_ed25519_key"
+      /etc/ssh/ssh_host_ed25519_key
       "${homeDir}/.ssh/id_ed25519"
     ];
   };
