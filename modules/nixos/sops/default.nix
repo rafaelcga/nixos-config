3dde22dffc54a42ef7879052e3461d4a0cb63b29
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-13 13:20:15 +0200

    update keys

--- modules/nixos/sops/default.nix
+++ modules/nixos/sops/default.nix
@@ -13,6 +13,7 @@ in
     age.sshKeyPaths = [
       "/etc/ssh/ssh_host_ed25519_key"
       "${homeDir}/.ssh/id_ed25519"
+      "/tmp/ssh/id_ed25519" # tmp location for local installs
     ];
     secrets = {
       user_password.neededForUsers = true;
