1ec983ffc0d62373d2fd6912ba322ac3f03e536d
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-14 10:14:43 +0200

    update sops

--- modules/nixos/sops/default.nix
+++ modules/nixos/sops/default.nix
@@ -6,15 +6,16 @@
 }:
 let
   homeDir = config.users.users.${userName}.home;
+  potentialSshKeyPaths = [
+    "/etc/ssh/ssh_host_ed25519_key"
+    "${homeDir}/.ssh/id_ed25519"
+    "/tmp/ssh/id_ed25519" # tmp location for local installs
+  ];
 in
 {
   sops = {
     defaultSopsFile = ../../../hosts/${hostName}/secrets.yaml;
-    age.sshKeyPaths = [
-      "/etc/ssh/ssh_host_ed25519_key"
-      "${homeDir}/.ssh/id_ed25519"
-      "/tmp/ssh/id_ed25519" # tmp location for local installs
-    ];
+    age.sshKeyPaths = builtins.filter builtins.pathExists potentialSshKeyPaths;
     secrets = {
       user_password.neededForUsers = true;
     };
