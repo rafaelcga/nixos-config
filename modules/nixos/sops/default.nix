919e3a0f299d51b91b4d313b4b01cca123317037
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-18 13:22:37 +0200

    (sops) try to fix sops cli

--- modules/nixos/sops/default.nix
+++ modules/nixos/sops/default.nix
@@ -6,18 +6,21 @@
   ...
 }:
 let
-  homeDir = config.users.users.${userName}.home;
+  privateKeyPath = "${config.users.users.${userName}.home}/.ssh/id_ed25519";
 in
 {
   sops = {
     defaultSopsFile = "${inputs.self}/secrets/${hostName}/secrets.yaml";
     age.sshKeyPaths = [
       "/etc/ssh/ssh_host_ed25519_key"
-      "${homeDir}/.ssh/id_ed25519"
+      privateKeyPath
       "/tmp/ssh/id_ed25519" # tmp location for local installs
     ];
     secrets = {
       user_password.neededForUsers = true;
     };
+  };
+  environment.sessionVariables = {
+    SOPS_AGE_SSH_PRIVATE_KEY_FILE = privateKeyPath; # ensure sops CLI works
   };
 }
