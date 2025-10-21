bb2da84d1e10709a110a55d8a1cfd47741abc250
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-21 21:16:39 +0200

    (refactor) optional user home config if exists

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -1,6 +1,12 @@
-{ inputs, config, ... }:
+{
+  inputs,
+  config,
+  lib,
+  ...
+}:
 let
   inherit (config.modules.nixos) user;
+  userHomeConfig = "${inputs.self}/home/${user.name}";
 in
 {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
@@ -9,7 +15,8 @@ in
     users.${user.name} = {
       imports = [
         "${inputs.self}/home/base.nix"
-      ];
+      ]
+      ++ lib.optionals (builtins.pathExists userHomeConfig) [ userHomeConfig ];
       home = { inherit (config.system) stateVersion; };
     };
 
