1400a5c7297783fb347945f959bf8a26282ec669
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-21 21:25:48 +0200

    fix(actions)

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,14 +1,15 @@
 { inputs, ... }:
 {
   imports = [
+    # External modules
     inputs.disko.nixosModules.disko
     ./home-manager.nix
 
+    # Local modules
     ./hardware
     ./system
-
     ./i18n.nix
-    ./user.nix
     ./sops.nix
+    ./user.nix
   ];
 }
