41f01e552c0971949850f1d4e411394a4cc1d873
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-22 12:33:01 +0200

    flake-parts

--- parts/overlays.nix
+++ parts/overlays.nix
@@ -1,22 +1,14 @@
 { inputs, ... }:
 {
+  flake.overlays.default = { };
+
   perSystem =
     { system, ... }:
     {
       _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
-        overlays = [
-          # TODO: Import overlays
-          (final: _: {
-            local = import "${inputs.self}/pkgs" {
-              inherit (inputs.nixpkgs) lib;
-              pkgs = final;
-            };
-          })
-        ];
-        config = {
-          allowUnfree = true;
-        };
+        overlays = [ inputs.self.overlays.default ];
+        config.allowUnfree = true;
       };
     };
 }
