95f32c91eb57b741a4a1c1f5e8e6f0cd58cf4049
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-23 01:42:23 +0200

    (sops) non-passphrase protected key

--- modules/nixos/external/sops-nix.nix
+++ modules/nixos/external/sops-nix.nix
@@ -35,11 +35,6 @@ in
       sops
     ];
 
-    sops.secrets."passwords/services" = { };
-    sops.templates."ssh-env".content = ''
-      SSH_PASSPHRASE=${config.sops.placeholder."passwords/services"}
-    '';
-
     # Generate age key file from private SSH
     systemd.services.generate-sops-age-key = {
       description = "Generates age key from private SSH for sops CLI";
@@ -48,19 +43,12 @@ in
         Type = "oneshot";
         User = user.name;
         Group = user.group;
-        EnvironmentFile = config.sops.templates."ssh-env".path;
       };
       script = ''
         set -euo pipefail
 
-        # Make temporary key with no passphrase
-        TMP_KEY=$(mktemp)
-        chmod 600 "$TMP_KEY"
-        cp "${user.sshPrivateKey}" "$TMP_KEY"
-        ssh-keygen -p -P "$SSH_PASSPHRASE" -N "" -f "$TMP_KEY"
-
         mkdir -p "$(dirname "${cfg.ageKeyFile}")"
-        ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i "$TMP_KEY" > "${cfg.ageKeyFile}"
+        ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i "${user.sshPrivateKey}" > "${cfg.ageKeyFile}"
       '';
     };
   };
