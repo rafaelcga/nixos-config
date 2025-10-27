843fff78427043811a406d94bda73f50f718796e
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-27 18:29:37 +0100

    (modules) lutris 2

---
+++ modules/home-manager/apps/lutris.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}:
+let
+  inherit (osConfig.programs) steam;
+  cfg = config.modules.home-manager.lutris;
+in
+{
+  options.modules.home-manager.lutris = {
+    enable = lib.mkEnableOption "Toggle module";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.lutris = {
+      enable = true;
+      steamPackage = lib.mkIf steam.enable steam.package;
+      extraPackages = with pkgs; [
+        umu-launcher
+      ];
+    };
+  };
+}
