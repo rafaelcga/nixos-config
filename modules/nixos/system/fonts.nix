49ceadc6819f5a0a3fd1c18e598836fbb0472292
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-28 17:20:00 +0100

    (fonts) can't install all nerd-fonts

--- modules/nixos/system/fonts.nix
+++ modules/nixos/system/fonts.nix
@@ -14,12 +14,13 @@ in
 
   config = lib.mkIf cfg.enable {
     fonts.packages = with pkgs; [
-      nerd-fonts
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
       noto-fonts-color-emoji
       liberation_ttf
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.zed-mono
     ];
   };
 }
