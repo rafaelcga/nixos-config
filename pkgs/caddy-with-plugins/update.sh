6e4cbf55d46a91e1a455100cadcf09055b7b83ca
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-29 22:39:47 +0100

    (caddy) move package

---
+++ pkgs/caddy-with-plugins/update.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p curl jq
+
+set -euo pipefail
+
+ROOT_DIR=$(dirname "$0")
+
+update_version() {
+  local updated_plugin=$(sed -E "s/@v[\.0-9]+/@$2/" <<<"$1")
+  echo "$updated_plugin"
+}
+
+update_plugins() {
+  for plugin in $(grep -oP "github.com/([a-zA-Z0-9_-]+/?)+@v[\.\d]+" "$ROOT_DIR/package.nix"); do
+    local parts=($(sed "s/[/@]/\n/g" <<<"$plugin"))
+
+    local old_version="${parts[-1]}"
+    local new_version=$(
+      curl -sL "https://api.github.com/repos/${parts[1]}/${parts[2]}/releases/latest" \
+        | jq -r ".tag_name"
+    )
+
+    if [[ "$old_version" != "$new_version" ]]; then
+      update_version "$plugin" "$new_version"
+    else
+      update_version "$plugin" "v0.0.0" # DEBUG
+    fi
+  done
+}
+
+update_hash() {
+  echo
+}
+
+update_plugins
