a3208da5138e96c6e965c09ce3bca934c3774928
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-10-30 19:26:06 +0100

    (scripts) update scripts completed

--- pkgs/caddy-with-plugins/update.sh
+++ pkgs/caddy-with-plugins/update.sh
@@ -1,80 +1,67 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p curl jq
+#!nix-shell --quiet -i bash -p curl jq
 
 set -euo pipefail
 
 ROOT_DIR="$(dirname "$(readlink -f "$0")")"
 
-update_plugins() {
-  echo "Checking for Caddy plugin updates..."
+echo "Checking for Caddy plugin updates..."
 
-  grep -oP "github.com/([a-zA-Z0-9_\-]+/?)+@[^\"]+" "$ROOT_DIR/package.nix" \
-    | while read plugin; do
-      local parts=($(sed "s|[/@]|\n|g" <<<"$plugin"))
-      local plugin_name=$(grep -oP "(?<=github.com/)([a-zA-Z0-9_\-]+/?)+(?=@.+)" <<<"$plugin")
-      printf "* %s... " "$plugin_name"
+grep -oP "github.com/([a-zA-Z0-9_\-]+/?)+@[^\"]+" "$ROOT_DIR/package.nix" \
+  | while read plugin; do
+    parts=($(sed "s|[/@]|\n|g" <<<"$plugin"))
+    plugin_name=$(grep -oP "(?<=github.com/)([a-zA-Z0-9_\-]+/?)+(?=@.+)" <<<"$plugin")
+    printf " %s " "$plugin_name"
 
-      local old_version="${parts[-1]}"
-      local new_version=$(
-        curl -sL "https://api.github.com/repos/${parts[1]}/${parts[2]}/releases/latest" \
-          | jq -r ".tag_name"
-      )
-
-      if [[ -z "$new_version" ]]; then
-        echo "[!] ERROR: Could not find latest version."
-        continue
-      fi
-
-      if [[ "$old_version" != "$new_version" ]]; then
-        local updated_plugin=$(sed -E "s|$old_version|$new_version|" <<<"$plugin")
-        sed -i "s|$plugin|$updated_plugin|" "$ROOT_DIR/package.nix"
-        echo "[✔] UPDATE APPLIED: $old_version -> $new_version"
-      else
-        echo "[✔] UP-TO-DATE"
-      fi
-    done
-}
-
-update_hash() {
-  echo "Updating derivation hash..."
-
-  # Replace non-valid strings by a fake hash
-  fake_hash="sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
-  if ! grep -qP "hash\s*=\s*\"sha256-[A-Za-z0-9\+\/]+=\"" "$ROOT_DIR/package.nix"; then
-    echo "[!] WARNING: Non-valid string found in hash, replacing with fake hash \"$fake_hash\"."
-    sed -i "s|\(hash\s*=\s*\"\).*\(\";\)|\1$fake_hash\2|" "$ROOT_DIR/package.nix"
-  fi
-
-  set +e
-  local output
-  output=$(nix-build -E \
-    "with import <nixpkgs> {}; callPackage $ROOT_DIR/package.nix {}" 2>&1)
-
-  if [[ $? -ne 0 ]]; then
-    set -e
-    old_hash=$(
-      echo "$output" \
-        | grep -oP "specified:\s*sha256-[A-Za-z0-9\+\/]+=" \
-        | sed -E "s|^specified:\s*||"
+    old_version="${parts[-1]}"
+    new_version=$(
+      curl -sL "https://api.github.com/repos/${parts[1]}/${parts[2]}/releases/latest" \
+        | jq -r ".tag_name"
     )
-    new_hash=$(
-      echo "$output" \
-        | grep -oP "got:\s*sha256-[A-Za-z0-9\+\/]+=" \
-        | sed -E "s|^got:\s*||"
-    )
-    sed -i "s|$old_hash|$new_hash|" "$ROOT_DIR/package.nix"
-    echo "[✔] UPDATED HASH:"
-    echo "  - $old_hash"
-    echo "  + $new_hash"
-  else
-    echo "[✔] HASH ALREADY UP-TO-DATE"
-  fi
-}
 
-update_plugins
+    if [[ -z "$new_version" ]]; then
+      echo "[❌] error: Could not find latest version."
+      continue
+    fi
 
-echo ""
-echo "--------------------------------------------------"
-echo ""
+    if [[ "$old_version" != "$new_version" ]]; then
+      updated_plugin=$(sed -E "s|$old_version|$new_version|" <<<"$plugin")
+      sed -i "s|$plugin|$updated_plugin|" "$ROOT_DIR/package.nix"
+      echo "[✔️] updated: $old_version -> $new_version"
+    else
+      echo "[✔️] up-to-date"
+    fi
+  done
 
-update_hash
+echo "Updating derivation hash..."
+
+# Replace non-valid strings by a fake hash
+fake_hash="sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+if ! grep -qP "hash\s*=\s*\"sha256-[A-Za-z0-9\+\/]+=\"" "$ROOT_DIR/package.nix"; then
+  echo "[❗] WARNING: Non-valid string found in hash, replacing with fake hash \"$fake_hash\"."
+  sed -i "s|\(hash\s*=\s*\"\).*\(\";\)|\1$fake_hash\2|" "$ROOT_DIR/package.nix"
+fi
+
+set +e
+output=$(nix-build -E \
+  "with import <nixpkgs> {}; callPackage $ROOT_DIR/package.nix {}" 2>&1)
+
+if [[ $? -ne 0 ]]; then
+  set -e
+  old_hash=$(
+    echo "$output" \
+      | grep -oP "specified:\s*sha256-[A-Za-z0-9\+\/]+=" \
+      | sed -E "s|^specified:\s*||"
+  )
+  new_hash=$(
+    echo "$output" \
+      | grep -oP "got:\s*sha256-[A-Za-z0-9\+\/]+=" \
+      | sed -E "s|^got:\s*||"
+  )
+  sed -i "s|$old_hash|$new_hash|" "$ROOT_DIR/package.nix"
+  echo " [✔️] Updated hash:"
+  echo "  - $old_hash"
+  echo "  + $new_hash"
+else
+  echo " [✔️] Hash already up-to-date"
+fi
--- scripts/update_pkgs.sh
+++ scripts/update_pkgs.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p nix-update
+#!nix-shell --quiet -i bash -p nix-update
 
 set -euo pipefail
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
 PKGS_DIR="$REPO_DIR/pkgs"
 
+tput bold
+echo "Upgrading local packages..."
+tput sgr0
 grep -P "(pkgs\.)?callPackage" "$PKGS_DIR/default.nix" \
   | while read line; do
     pkg_name="$(grep -oP "^\s*\K[a-zA-Z0-9_\-]+" <<<"$line")"
@@ -14,11 +17,27 @@ grep -P "(pkgs\.)?callPackage" "$PKGS_DIR/default.nix" \
     abs_path="$(readlink -m "$PKGS_DIR/$rel_path")"
     pkg_dir="$(dirname "$abs_path")"
 
-    # Execute updates in sub-shells
+    tput bold
+    printf "* %s " "$pkg_name"
+    tput sgr0
     if [[ "$(basename "$abs_path")" == "package.nix" ]] \
       && [[ -f "$pkg_dir/update.sh" ]]; then
+      echo "through custom script..."
       (cd "$pkg_dir" && ./update.sh)
     else
-      (cd "$REPO_DIR" && nix-update "$pkg_name" --flake)
+      set +e
+      (
+        cd "$REPO_DIR"
+        update_line="$(
+          nix-update "$pkg_name" --flake 2>&1 \
+            | grep -oP "Update \K\S+ -> \S+(?= in $abs_path)"
+        )"
+        if [[ -z $update_line ]]; then
+          echo "[✔️️] up-to-date"
+        else
+          echo "[✔️️] updated: $update_line"
+        fi
+      )
+      set -e
     fi
   done
