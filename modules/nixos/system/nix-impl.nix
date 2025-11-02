b10f0c7d93216eccae4fdd926f72cb82388e58e9
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-11-03 00:09:16 +0100

    (nixpkgs) add my cachix repo

--- modules/nixos/system/nix-impl.nix
+++ modules/nixos/system/nix-impl.nix
@@ -54,9 +54,11 @@ in
           # Lower value means higher priority; default is 40
           "https://cache.nixos.org?priority=10"
           "https://nix-community.cachix.org"
+          "https://rafaelcga.cachix.org"
         ];
         trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "rafaelcga.cachix.org-1:rwBDDlTDx0rxoecuMjIx6yxPQU+0nc49Y7d04OaklG0="
         ];
       };
       # Uses programs.nh.clean as garbage collector instead
