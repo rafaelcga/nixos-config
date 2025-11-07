e4a9f316f5167fe947992cc57f11450a5b2612a0
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-11-07 22:08:13 +0100

    prepare for merge

--- hosts/fractal/default.nix
+++ hosts/fractal/default.nix
@@ -18,6 +18,7 @@
         "nvidia"
       ];
     };
+    printing.enable = true;
     # External modules
     disko.disks = {
       main = {
@@ -34,52 +35,6 @@
     flatpak.enable = true;
     # Apps
     steam.enable = true;
-    virt-manager.enable = true;
-    # Containers
-    containers = {
-      externalInterface = "enp1s0";
-      hostAddress = "172.22.0.1";
-      hostAddress6 = "fc00::1";
-      # WebUI port range 8000-8999
-      instances = {
-        adguardhome = {
-          enable = true;
-          localAddress = "172.22.0.2";
-          localAddress6 = "fc00::2";
-          hostPort = 8000;
-        };
-        ddns-updater = {
-          enable = true;
-          localAddress = "172.22.0.3";
-          localAddress6 = "fc00::3";
-          hostPort = 8001;
-        };
-        jellyfin = {
-          enable = true;
-          localAddress = "172.22.0.4";
-          localAddress6 = "fc00::4";
-          hostPort = 8002;
-          gpuPassthrough = true;
-          bindMounts = {
-            "/media" = {
-              hostPath = "/mnt/media";
-              isReadOnly = true;
-            };
-          };
-        };
-        servarr = {
-          enable = true;
-          localAddress = "172.22.0.5";
-          localAddress6 = "fc00::5";
-          hostPorts = {
-            lidarr = 8003;
-            radarr = 8004;
-            sonarr = 8005;
-            prowlarr = 8006;
-          };
-        };
-      };
-    };
   };
 
   environment.sessionVariables.GDK_SCALE = "1.25"; # sets XWayland render scale
