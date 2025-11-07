4cd49a0f01872a52f16690ae3421e7fa27821ed4
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-11-07 20:46:33 +0100

    (containers) finally refactored

--- modules/nixos/containers/ddns-updater.nix
+++ modules/nixos/containers/ddns-updater.nix
@@ -1,1 +1,51 @@
-{ }
+{ config, lib, ... }:
+let
+  cfg = config.modules.nixos.containers.instances.ddns-updater;
+
+  secrets = {
+    "web_domain" = { };
+    "porkbun/api_key" = { };
+    "porkbun/api_secret_key" = { };
+  };
+  # https://github.com/qdm12/ddns-updater/blob/master/docs/porkbun.md
+  jsonConfig = ''
+    {
+      "settings": [
+        {
+          "provider": "porkbun",
+          "domain": "*.${config.sops.placeholder."web_domain"}",
+          "api_key": "${config.sops.placeholder."porkbun/api_key"}",
+          "secret_api_key": "${config.sops.placeholder."porkbun/api_secret_key"}"
+        }
+      ]
+    }
+  '';
+in
+lib.mkMerge [
+  { modules.nixos.containers.instances.ddns-updater.containerPort = 8000; }
+  (lib.mkIf cfg.enable {
+    sops = {
+      inherit secrets;
+      templates."ddns-updater/config.json".content = jsonConfig;
+    };
+
+    containers.ddns-updater = {
+      bindMounts = {
+        "${config.sops.templates."ddns-updater/config.json".path}" = {
+          isReadOnly = true;
+        };
+      };
+
+      config = {
+        services.ddns-updater = {
+          enable = true;
+          environment = {
+            CONFIG_FILEPATH = config.sops.templates."ddns-updater/config.json".path;
+            LISTENING_ADDRESS = ":${builtins.toString cfg.containerPort}";
+            TZ = "Europe/Madrid";
+          };
+        };
+      };
+    };
+  })
+]
