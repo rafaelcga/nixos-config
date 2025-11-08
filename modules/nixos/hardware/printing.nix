11896a58716bfd89e70a3b78fc0673617e91812b
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-11-08 16:09:27 +0100

    (printing) updated implementation

--- modules/nixos/hardware/printing.nix
+++ modules/nixos/hardware/printing.nix
@@ -6,10 +6,26 @@
 }:
 let
   cfg = config.modules.nixos.printing;
+
+  vendorDrivers = with pkgs; {
+    canon = [ cnijfilter2 ];
+    hp = [ hplipWithPlugin ];
+    samsung = [ samsung-unified-linux-driver ];
+  };
 in
 {
   options.modules.nixos.printing = {
     enable = lib.mkEnableOption "Enable printing support";
+
+    vendor = lib.mkOption {
+      type = lib.types.enum [
+        "canon"
+        "hp"
+        "samsung"
+      ];
+      default = "canon";
+      description = "Printer vendor";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -18,14 +34,15 @@ in
         enable = true;
         nssmdns4 = true;
         openFirewall = true;
+        publish = {
+          enable = true;
+          userServices = true;
+        };
       };
 
       printing = {
         enable = true;
-        drivers = with pkgs; [
-          cups-filters
-          cups-browsed
-        ];
+        drivers = vendorDrivers.${cfg.vendor};
       };
     };
   };
