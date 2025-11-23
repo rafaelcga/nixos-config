d870bf3754bf3b909cd0ea2191b8fcdd3cbd47d2
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-11-23 13:47:16 +0100

    (containers) fix error

--- modules/nixos/containers/default.nix
+++ modules/nixos/containers/default.nix
@@ -4,14 +4,18 @@
     ./servarr.nix
   ];
 
-  networking.nat = {
-    enable = true;
-    enableIPv6 = true;
-    internalInterfaces = [ (if config.networking.nftables.enable then "ve-*" else "ve-+") ];
-    externalInterface = config.modules.nixos.networking.defaultInterface;
-  };
+  networking = {
+    nat = {
+      enable = true;
+      enableIPv6 = true;
+      externalInterface = config.modules.nixos.networking.defaultInterface;
+      internalInterfaces = [ (if config.networking.nftables.enable then "ve-*" else "ve-+") ];
+    };
 
-  networkmanager = lib.mkIf config.networking.networkmanager.enable {
-    unmanaged = [ "interface-name:ve-*" ];
+    # Prevent NetworkManager from managing container interfaces
+    # https://nixos.org/manual/nixos/stable/#sec-container-networking
+    networkmanager = lib.mkIf config.networking.networkmanager.enable {
+      unmanaged = [ "interface-name:ve-*" ];
+    };
   };
 }
