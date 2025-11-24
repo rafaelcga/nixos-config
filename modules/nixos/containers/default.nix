a3f9da8bfa6b8c987487372a52bb36d1ab96a612
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-11-24 20:25:55 +0100

    (containers) remove 10. subnet

--- modules/nixos/containers/default.nix
+++ modules/nixos/containers/default.nix
@@ -79,7 +79,6 @@ in
       templates."${cfg.wireguardInterface}.conf".content =
         let
           localIpv4 = [
-            "10.0.0.0/8"
             "172.16.0.0/12"
             "192.168.0.0/16"
           ];
