dfe4753fdc7098056d205fd727721179573ed099
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-11-28 22:50:51 +0100

    (homepage) fix

--- modules/nixos/containers/services/homepage/service-data.nix
+++ modules/nixos/containers/services/homepage/service-data.nix
@@ -2,153 +2,104 @@
 let
   utils = import "${inputs.self}/lib/utils.nix" { inherit lib; };
 
-  serviceOpts =
-    { name, ... }:
-    {
-      displayName = lib.mkOption {
-        type = lib.types.str;
-        default = utils.capitalizeFirst name;
-        description = "Name to display for the service";
+  mkServiceData =
+    name: data:
+    let
+      defaultData = {
+        displayName = utils.capitalizeFirst name;
+        description = "";
+        icon = "${name}.svg";
+        type = name;
+        container = name;
+        apiAuth = null; # key or password
+        widgetFields = [ ];
+        extraConfig = { };
       };
-
-      description = lib.mkOption {
-        type = lib.types.str;
-        default = "";
-        description = "Service description";
-      };
-
-      icon = lib.mkOption {
-        type = lib.types.str;
-        default = name;
-        apply = name: "${name}.svg";
-        description = "Service icon";
-      };
-
-      type = lib.mkOption {
-        type = lib.types.str;
-        default = name;
-        description = "Homepage Dashboard service type";
-      };
-
-      container = lib.mkOption {
-        type = lib.types.str;
-        default = name;
-        description = "Container that holds the service";
-      };
-
-      apiAuth = lib.mkOption {
-        type = lib.types.nullOr (
-          lib.types.enum [
-            "key"
-            "password"
-          ]
-        );
-        default = null;
-        description = "Service API authentication method (key or password)";
-      };
-
-      widgetFields = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [ ];
-        description = "Fields to be displayed in the service widget";
-      };
-
-      extraConfig = lib.mkOption {
-        type = lib.types.attrs;
-        default = { };
-        description = "Extra attributes to configure the service entry";
-      };
-    };
+    in
+    defaultData // data;
 in
-{
-  options = lib.mkOption {
-    type = lib.types.attrsOf (lib.types.submodule serviceOpts);
-    default = { };
+lib.mapAttrs mkServiceData {
+  lidarr = {
+    description = "Music collection manager";
+    container = "servarr";
+    apiAuth = "key";
+    widgetFields = [
+      "wanted"
+      "queued"
+      "artists"
+    ];
   };
 
-  config = {
-    lidarr = {
-      description = "Music collection manager";
-      container = "servarr";
-      apiAuth = "key";
-      widgetFields = [
-        "wanted"
-        "queued"
-        "artists"
-      ];
-    };
+  radarr = {
+    description = "Movie collection manager";
+    container = "servarr";
+    apiAuth = "key";
+    widgetFields = [
+      "wanted"
+      "queued"
+      "movies"
+    ];
+  };
 
-    radarr = {
-      description = "Movie collection manager";
-      container = "servarr";
-      apiAuth = "key";
-      widgetFields = [
-        "wanted"
-        "queued"
-        "movies"
-      ];
-    };
+  sonarr = {
+    description = "TV series collection manager";
+    container = "servarr";
+    apiAuth = "key";
+    widgetFields = [
+      "wanted"
+      "queued"
+      "series"
+    ];
+  };
 
-    sonarr = {
-      description = "TV series collection manager";
-      container = "servarr";
-      apiAuth = "key";
-      widgetFields = [
-        "wanted"
-        "queued"
-        "series"
-      ];
-    };
+  prowlarr = {
+    description = "Indexer manager and proxy";
+    container = "servarr";
+    apiAuth = "key";
+    widgetFields = [
+      "numberOfGrabs"
+      "numberOfQueries"
+      "numberOfFailGrabs"
+      "numberOfFailQueries"
+    ];
+  };
 
-    prowlarr = {
-      description = "Indexer manager and proxy";
-      container = "servarr";
-      apiAuth = "key";
-      widgetFields = [
-        "numberOfGrabs"
-        "numberOfQueries"
-        "numberOfFailGrabs"
-        "numberOfFailQueries"
-      ];
+  qbittorrent = {
+    displayName = "VueTorrent";
+    description = "BitTorrent client";
+    icon = "vuetorrent";
+    apiAuth = "password";
+    widgetFields = [
+      "leech"
+      "download"
+      "seed"
+      "upload"
+    ];
+    extraConfig = {
+      enableLeechProgress = true;
+      enableLeechSize = true;
     };
+  };
 
-    qbittorrent = {
-      displayName = "VueTorrent";
-      description = "BitTorrent client";
-      icon = "vuetorrent";
-      apiAuth = "password";
-      widgetFields = [
-        "leech"
-        "download"
-        "seed"
-        "upload"
-      ];
-      extraConfig = {
-        enableLeechProgress = true;
-        enableLeechSize = true;
-      };
-    };
+  jellyfin = {
+    description = "Media streaming server";
+  };
 
-    jellyfin = {
-      description = "Media streaming server";
-    };
+  ddns-updater = {
+    displayName = "DNS Updater";
+    description = "Dynamic DNS record updater";
+  };
 
-    ddns-updater = {
-      displayName = "DNS Updater";
-      description = "Dynamic DNS record updater";
-    };
-
-    adguard = {
-      displayName = "AdGuard Home";
-      description = "Network-wide ad-blocking DNS server";
-      icon = "adguard-home";
-      apiAuth = "password";
-      widgetFields = [
-        "queries"
-        "blocked"
-        "filtered"
-        "latency"
-      ];
-    };
+  adguard = {
+    displayName = "AdGuard Home";
+    description = "Network-wide ad-blocking DNS server";
+    icon = "adguard-home";
+    apiAuth = "password";
+    widgetFields = [
+      "queries"
+      "blocked"
+      "filtered"
+      "latency"
+    ];
   };
 }
