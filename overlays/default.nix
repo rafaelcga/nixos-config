bbd3a1b4e44355f7a3a6f9ae537df6075652d048
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-11-29 01:09:38 +0100

    (homepage-dashboard) overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,7 @@ args@{ inputs, ... }:
 {
   nixpkgs.overlays = [
     (import ./catppuccin.nix args)
-    (import ./homepage-dashboard.nix args)
+    (import ./homepage-dashboard.nix)
     (final: prev: {
       local = import "${inputs.self}/pkgs" { pkgs = final; };
     })
