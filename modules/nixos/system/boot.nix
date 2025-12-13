dbe43acab193726579ac93180af5c79783c895eb
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2025-12-13 09:10:47 +0100

    (kernel) use vanilla linux

--- modules/nixos/system/boot.nix
+++ modules/nixos/system/boot.nix
@@ -18,7 +18,7 @@ in
 
   config = {
     boot = {
-      kernelPackages = pkgs.linuxPackages_zen;
+      kernelPackages = pkgs.linuxPackages_latest;
       initrd = {
         systemd.enable = true;
         verbose = true;
