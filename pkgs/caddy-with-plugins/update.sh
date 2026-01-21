4b065ff0bc15bd7efb1a3ebe39cffc667e4564d5
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2026-01-21 10:51:18 +0100

    (caddy) fix update script

--- pkgs/caddy-with-plugins/update.sh
+++ pkgs/caddy-with-plugins/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell --quiet -i bash -p coreutils gnused curl jq go
+#!nix-shell --quiet -i bash -p coreutils gnugrep gnused curl jq go
 
 set -euo pipefail
 
@@ -9,17 +9,18 @@ PKG_FILE="$ROOT_DIR/package.nix"
 
 echo "Checking for Caddy plugin updates..."
 
-grep -oP "github.com/\K([a-zA-Z0-9_\-]+/?)+@[^\"]+" "$PKG_FILE" \
+grep -oP "github.com/([a-zA-Z0-9_\-]+/?)+@[^\"]+" "$PKG_FILE" \
   | while read -r plugin; do
     printf " %s " "$plugin"
 
     plugin_path="${plugin%@*}"
     old_version="${plugin#*@}"
     # Keep only <author>/<repo_name>
-    plugin_repo="$(echo "$plugin_path" | cut -d'/' -f1-2)"
+    plugin_repo="$(echo "$plugin_path" | cut -d'/' -f1-3)"
 
     # Grep the plugin version
     {
+      rm -f go.mod go.sum
       go mod init temp && go get $plugin_path
     } >/dev/null 2>&1
 
