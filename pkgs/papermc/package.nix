2956a05c9b09e29a63bfff80741b746b0c1effcd
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2026-02-08 10:20:39 +0000

    chore: updated flake

--- pkgs/papermc/package.nix
+++ pkgs/papermc/package.nix
@@ -9,11 +9,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "papermc";
-  version = "1.21.11-104";
-  hash = "sha256-ApaR1DFP7+Vo1s9Zai3G/tyISgR8cutacf7oPDYxUiY=";
+  version = "1.21.11-111";
+  hash = "sha256-gqSHq4wtD28HK4267ckhjIWuOn0WeAgSRGKySJVb7sM=";
 
   src = fetchurl {
-    url = "https://fill-data.papermc.io/v1/objects/029691d4314fefe568d6cf596a2dc6fedc884a047c72eb5a71fee83c36315226/paper-1.21.11-104.jar";
+    url = "https://fill-data.papermc.io/v1/objects/82a487ab8c2d0f6f072b8dbaedc9218c85ae3a7d167808124462b248955beec3/paper-1.21.11-111.jar";
     inherit (finalAttrs) hash;
   };
 
