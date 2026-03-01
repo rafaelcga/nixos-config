ca8241decebaa05647376b72e1473dcddd03120a
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2026-03-01 00:23:55 +0000

    chore: updated flake

--- pkgs/caddy-with-plugins/package.nix
+++ pkgs/caddy-with-plugins/package.nix
@@ -2,13 +2,13 @@
 let
   plugins = [
     "github.com/caddy-dns/porkbun@v0.3.1"
-    "github.com/hslatman/caddy-crowdsec-bouncer/http@v0.9.2"
-    "github.com/hslatman/caddy-crowdsec-bouncer/appsec@v0.9.2"
-    "github.com/hslatman/caddy-crowdsec-bouncer/layer4@v0.9.2"
+    "github.com/hslatman/caddy-crowdsec-bouncer/http@v0.10.0"
+    "github.com/hslatman/caddy-crowdsec-bouncer/appsec@v0.10.0"
+    "github.com/hslatman/caddy-crowdsec-bouncer/layer4@v0.10.0"
   ];
 in
 caddy.withPlugins {
   inherit plugins;
-  hash = "sha256-n6rlWb6xKiKv4ii3yxvkldznhRgW1qkPh1bOt7OOxZs=";
+  hash = "sha256-wdq4UBAF8R9Ju3P2xszAA5jqRrWVQdtqo70wu0QIsLc=";
   doInstallCheck = false;
 }
