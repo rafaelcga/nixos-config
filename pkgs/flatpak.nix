11d330ee7af63a52b753a2e983640d27b8dbd887
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2026-04-15 13:32:02 +0200

    (flatpak) fix bug

--- pkgs/flatpak.nix
+++ pkgs/flatpak.nix
@@ -3,11 +3,11 @@
   fetchurl,
 }:
 
-flatpak.overrideAttrs (finalAttrs: {
+flatpak.overrideAttrs (finalAttrs: rec {
   version = "1.17.6";
 
   src = fetchurl {
-    url = "https://github.com/flatpak/flatpak/releases/download/${finalAttrs.version}/flatpak-${finalAttrs.version}.tar.xz";
-    hash = "sha256-dh/zugDJmib5FMaZnpCxKlTKsZzqWIhBPxfkbuYY2P4=";
+    url = "https://github.com/flatpak/flatpak/releases/download/${version}/flatpak-${version}.tar.xz";
+    hash = "sha256-pYzO1cRoeSwed4RxWeaH0/e+3owtRIPx8+8SKdUlU7I=";
   };
 })
