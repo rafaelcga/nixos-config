07660555e59f706d844c407d16503c09bd5c7ae1
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2026-07-31 13:05:40 +0200

    (containers) added trilium server

---
+++ modules/nixos/containers/services/trilium.nix
@@ -0,0 +1,31 @@
+{ config, lib, ... }:
+let
+  cfg = config.modules.nixos.containers.services.trilium;
+  configModules = config.modules.nixos;
+in
+lib.mkMerge [
+  {
+    modules.nixos.containers.services.trilium = {
+      uid = 9;
+      containerPort = 8080;
+      dataDir = "/var/lib/trilium";
+    };
+  }
+  (lib.mkIf cfg.enable {
+    containers.trilium = {
+      config = {
+        services.trilium-server = {
+          inherit (cfg) enable dataDir;
+          port = cfg.containerPort;
+        };
+      };
+    };
+
+    modules.nixos.caddy = lib.mkIf configModules.caddy.enable {
+      virtualHosts.trilium = {
+        originHost = cfg.address;
+        originPort = cfg.containerPort;
+      };
+    };
+  })
+]
