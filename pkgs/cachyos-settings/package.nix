b1bcdb738eaf11930a02bd5671e78d9d251f40c5
rafaelcga <68070715+rafaelcga@users.noreply.github.com>
2026-08-10 10:59:25 +0000

    chore: updated flake

--- pkgs/cachyos-settings/package.nix
+++ pkgs/cachyos-settings/package.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cachyos-settings";
-  version = "1.3.5";
+  version = "1.4.0";
 
   src = fetchFromGitHub {
     owner = "CachyOS";
     repo = "CachyOS-Settings";
     tag = version;
-    sha256 = "sha256-hAYnrfzqwLbi7Y/nrJgy4voZ+K3Tvx+Dt35JS+kizQc=";
+    sha256 = "sha256-0wDX3XOZTVLRqhZJfAg2lsUZLOrAbvxLOoPmkiFIia0=";
   };
 
   dontBuild = true;
